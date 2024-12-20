befc547da78307321f3b3d6f567df086695c3206
N H <nh@MBP.local>
2024-12-20 21:24:28 +0900

    チャットグループ一覧画面作成

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+// Providers
+import '../providers/authentication_provider.dart';
+import '../providers/chats_page_provider.dart';
+
+// Widget
+import '../widgets/top_bar.dart';
+import '../widgets/custom_list_view_tiles.dart';
+
+// Models
+import '../models/chat.dart';
+import '../models/chat_user.dart';
+import '../models/chat_message.dart';
+
 class ChatsPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -10,14 +23,110 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+  late ChatsPageProvider _pageProvider;
+
   @override
   Widget build(BuildContext context) {
-    return _buildUI();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ChatsPageProvider>(
+          create: (_) => ChatsPageProvider(_auth),
+        ),
+      ],
+      child: _buildUI(),
+    );
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      backgroundColor: Colors.red,
+    return Builder(builder: (BuildContext _context) {
+      _pageProvider = _context.watch<ChatsPageProvider>();
+      return Container(
+        padding: EdgeInsets.symmetric(
+          vertical: _deviceHeight * 0.02,
+          horizontal: _deviceWidth * 0.03,
+        ),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TopBar(
+              'Chats',
+              primaryAction: IconButton(
+                icon: Icon(Icons.logout),
+                color: Color.fromRGBO(0, 82, 218, 1.0),
+                onPressed: () {
+                  _auth.logout();
+                },
+              ),
+            ),
+            _chatsList(),
+          ],
+        ),
+      );
+    });
+  }
+
+  Widget _chatsList() {
+    List<Chat>? _chats = _pageProvider.chats;
+    return Expanded(
+      child: (() {
+        if (_chats != null) {
+          if (_chats.length != 0) {
+            return ListView.builder(
+              itemCount: _chats.length,
+              itemBuilder: (BuildContext _context, int _index) {
+                return _chatTile(_chats[_index]);
+              },
+            );
+          } else {
+            return Center(
+              child: Text(
+                "No chats found",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            );
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          );
+        }
+      })(),
+    );
+  }
+
+  Widget _chatTile(Chat _chat) {
+    List<ChatUser> _recepients = _chat.recepients();
+    bool _isActive = _recepients.any((_d) => _d.wasRecentlyActive());
+    String _subtitleText = "";
+    if (_chat.messages.isNotEmpty) {
+      _subtitleText = _chat.messages.first.type != MessageType.TEXT
+          ? "Media attachment"
+          : _chat.messages.first.content;
+    }
+    return CustomListViewTilesWithActivity(
+      height: _deviceHeight * 0.10,
+      title: _chat.title(),
+      subtitle: _subtitleText,
+      imagePath: "https://i.pravatar.cc/300",
+      isActive: _isActive,
+      isActivity: _chat.activity,
+      onTap: () {},
     );
   }
 }
---
+++ lib/widgets/custom_list_view_tiles.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+// Widgets
+import '../widgets/rounded_image.dart';
+
+// Models
+import '../models/chat_message.dart';
+import '../models/chat_user.dart';
+
+class CustomListViewTilesWithActivity extends StatelessWidget {
+  final double height;
+  final String title;
+  final String subtitle;
+  final String imagePath;
+  final bool isActive;
+  final bool isActivity;
+  final Function onTap;
+
+  CustomListViewTilesWithActivity({
+    required this.height,
+    required this.title,
+    required this.subtitle,
+    required this.imagePath,
+    required this.isActive,
+    required this.isActivity,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        onTap: () => onTap(),
+        minVerticalPadding: height * 0.20,
+        leading: RoundedImageNetworkWithStatusIndcator(
+          key: UniqueKey(),
+          imagePath: imagePath,
+          size: height / 2,
+          isActive: isActive,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        subtitle: isActivity
+            ? Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SpinKitThreeBounce(
+                    color: Colors.white54,
+                    size: height * 0.10,
+                  )
+                ],
+              )
+            : Text(
+                subtitle,
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ));
+  }
+}
